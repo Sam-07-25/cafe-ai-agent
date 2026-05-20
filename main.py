2dfba0b07fa3bdec081f1d3dfe25dd4da3227a1a
Sam-07-25 <samantha.dominguezf@gmail.com>
2026-05-20 16:12:49 -0600

    docs
    update internal code comments

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import os
 from groq import Groq
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() # loads API key from .env file
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+client = Groq(api_key=os.getenv("GROQ_API_KEY")) # connection to Groq's servers
 
 response = client.chat.completions.create(
-    model="llama-3.3-70b-versatile",
+    model="llama-3.3-70b-versatile", # which model to use
     messages=[
         {"role": "user", "content": "What are 3 common problems cafe owners face?"}
-    ]
-)
+    ] # conversation (one user message)
+) # POST request to Groq's servers
 
-print(response.choices[0].message.content)
+print(response.choices[0].message.content) # response is a JSON onject full with metadata, so we access the text only
