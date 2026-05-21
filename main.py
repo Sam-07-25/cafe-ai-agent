54be0f97a0c575254dd0da4e159761bd332cf710
Sam-07-25 <samantha.dominguezf@gmail.com>
2026-05-20 19:17:05 -0600

    refactor
    use langchain to rewrite main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import os
-from groq import Groq
 from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.messages import SystemMessage, HumanMessage
 
-load_dotenv() # loads API key from .env file
-client = Groq(api_key=os.getenv("GROQ_API_KEY")) # connection to Groq's servers
+load_dotenv()
 
-user_input = input("Ask something: ")
-response = client.chat.completions.create(
-    model="llama-3.3-70b-versatile", # which model to use
-    messages=[
-        {"role": "user", "content": user_input},
-    ] # conversation (one user message)
-) # POST request to Groq's servers
+llm = ChatGroq(
+    api_key=os.getenv("GROQ_API_KEY"),
+    model="llama-3.3-70b-versatile"
+)
 
-print(response.choices[0].message.content) # response is a JSON onject full with metadata, so we access the text only
+messages = [
+    SystemMessage(content="You are a friendly assistant for a small cafe in Mexico."),
+    HumanMessage(content="What are 3 common problems cafe owners face?")
+]
+
+response = llm.invoke(messages)
+print(response.content)
