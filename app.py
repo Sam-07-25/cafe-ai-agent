ed3e96f37fbb31149653414646d8b43884c5aed1
Sam-07-25 <samantha.dominguezf@gmail.com>
2026-06-04 18:37:34 -0600

    Update app.py
    delete get_menu() and update llm instructions

--- app.py
+++ app.py
@@ -14,30 +14,21 @@ app = Flask(__name__) # creates Flask web application
 
 # builds groq-based llm
 llm = ChatGroq(
-    api_key=os.getenv("GROQ_API_KEY"),
-    model="llama-3.3-70b-versatile"
+    api_key = os.getenv("GROQ_API_KEY"),
+    model = "llama-3.3-70b-versatile"
 )
-
-# retrieves menu
-@tool
-def get_menu() -> str:
-    """Returns the cafe menu with items and prices."""
-    return """
-    - Americano: $45
-    - Cappuccino: $55
-    - Latte: $55
-    - Oat milk latte: $65
-    - Croissant: $35
-    - Avocado toast: $75
-    """
 
 tools = all_tools # list of available tools
 
 # creates react agent using llm, tools, and specs
 agent = create_react_agent(
-    model=llm,
-    tools=tools,
-    prompt="You are a friendly assistant for Café Tres Leches in Chihuahua. You have access to a get_menu tool. ONLY use the get_menu tool when the customer EXPLICITLY asks for the menu, food items, drinks, or prices. NEVER use the get_menu tool for greetings or general conversation. When you retrieve the menu, always display the complete list of items and prices. Respond in Spanish always."
+    model = llm,
+    tools = tools,
+    prompt = """
+    You are a friendly assistant for Café Tres Leches in El Paso. You have access to many tools that can help you retrieve information
+    about the cafe or make and cancel reservations. You must display the strings that are returned from these tools when you think
+    appropriate.
+    """
 )
 
 conversation_histories = {} # stores conversation history per user
